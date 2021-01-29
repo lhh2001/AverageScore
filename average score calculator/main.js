6cd670ee4fbadfdf52f578563a5af6c911fb5e50
lhh2001 <lihenghao2001@163.com>
2021-01-29 15:44:36 +0800

    添加显示成绩的表格, 修复更改cookie时可能的BUG

--- average score calculator/main.js
+++ average score calculator/main.js
@@ -3,6 +3,7 @@ var loading = document.getElementById("loading");
 var scoreData = null;
 var semesterArray = new Array();
 var exceptionText = "查询成绩异常! 请检查是否登录并打开成绩查询页面, 重启本程序";
+var tableHead = "<tr><th>课程</th><th>学分</th><th>绩点</th><th>成绩</th></tr>";
 
 // 程序所需的cookie
 var weu = null;
@@ -90,6 +91,9 @@ function getScoreData()
 		if (xhr.responseText[0] != '{')
 		{
 			loading.textContent = exceptionText;
+			//请求失败, 删除已设置的Cookie防止污染
+			chrome.cookies.remove({url: "http://ehall.xjtu.edu.cn/jwapp/", name: "_WEU"}, function(){});
+			chrome.cookies.remove({url: "http://ehall.xjtu.edu.cn/", name: "MOD_AMP_AUTH"}, function(){});
 			return;
 		}
 		main.style.display = "flex";
@@ -133,16 +137,14 @@ function calAverageScore(scoreData, isMajorOnly, semester)
 {
 	let totalScore = 0.0;
 	let totalCredit = 0.0;
-	
+
+	if (!isMajorOnly)
+	{
+		document.getElementById("score-table-body").innerHTML = tableHead;
+	}
+
 	for (let i = 0; i < scoreData.length; i++)
 	{
-		if (isMajorOnly)
-		{
-			if (scoreData[i]["KCH"].indexOf("CORE") != -1 || scoreData[i]["KCH"].indexOf("GNED") != -1)
-			{
-				continue;
-			}
-		}
 		if (semester != "all")
 		{
 			if (scoreData[i]["XNXQDM"] != semester)
@@ -150,8 +152,30 @@ function calAverageScore(scoreData, isMajorOnly, semester)
 				continue;
 			}
 		}
+		
+		if (isMajorOnly)
+		{
+			if (scoreData[i]["KCH"].indexOf("CORE") != -1 || scoreData[i]["KCH"].indexOf("GNED") != -1)
+			{
+				continue;
+			}
+		}
+		else //由于本函数执行两遍, 防止重复添加元素
+		{
+			//新建表格的一行
+			let textData = [scoreData[i]["KCM"], scoreData[i]["XF"], scoreData[i]["XFJD"], scoreData[i]["ZCJ"]];
+			let tr = document.createElement("tr");
+			for (let j = 0; j < textData.length; j++)
+			{
+				let td = document.createElement("td");
+				td.textContent = textData[j];
+				tr.appendChild(td);
+			}
+			document.getElementById("score-table-body").appendChild(tr);
+		}
+
 		totalScore += scoreData[i]["ZCJ"] * parseFloat(scoreData[i]["XF"]);
-        totalCredit += parseFloat(scoreData[i]["XF"]);
+		totalCredit += parseFloat(scoreData[i]["XF"]);
 	}
 
 	if (totalCredit == 0)
